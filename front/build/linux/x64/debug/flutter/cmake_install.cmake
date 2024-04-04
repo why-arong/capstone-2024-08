c612328508589d8cefebd1c0573391fd9b1b22b2
Shin-MG <alsruddl0407@kookmin.ac.kr>
2024-04-05 01:33:30 +0900

    [CD-87] Move: modify the path of .env file & add .env to pubspec.yaml

--- front/build/linux/x64/debug/flutter/cmake_install.cmake
+++ front/build/linux/x64/debug/flutter/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/shin/capstone/front/capstone/linux/flutter
+# Install script for directory: /home/shin/capstone/capstone-2024-08/front/linux/flutter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/shin/capstone/front/capstone/build/linux/x64/debug/bundle")
+  set(CMAKE_INSTALL_PREFIX "/home/shin/capstone/capstone-2024-08/front/build/linux/x64/debug/bundle")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:capstone/widget/bottom_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
-void main() {
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
