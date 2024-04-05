70af390c44abf14a4b830b79a3d9721cb3528ee0
Shin-MG <alsruddl0407@kookmin.ac.kr>
2024-04-06 00:44:46 +0900

    [CD-91] Feat: load example_script data from firestore

---
+++ front/lib/model/load_data.dart
@@ -0,0 +1,27 @@
+import 'package:capstone/model/script.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class LoadData {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  
+  Stream<List<ScriptModel>> readExampleScripts(String? category) {
+    if (category == '전체') {
+      return firestore
+          .collection('example_script')
+          .orderBy('createdAt', descending: true)
+          .snapshots()
+          .map((snapshot) => snapshot.docs
+              .map((doc) => ScriptModel.fromDocument(doc: doc))
+              .toList());
+    } else {
+      return firestore
+          .collection('example_script')
+          .where('category', isEqualTo: category)
+          .orderBy('createdAt', descending: true)
+          .snapshots()
+          .map((snapshot) => snapshot.docs
+              .map((doc) => ScriptModel.fromDocument(doc: doc))
+              .toList());
+    }
+  }
+}
