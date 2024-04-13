eac77a251e9be6e550c6232ffc55f77c86f4ab5d
Pilmo <pilmokim99@gmail.com>
2024-04-13 17:38:06 +0900

    [CD-109] Feat: add whisper

--- backend/feedback/stt.py
+++ backend/feedback/stt.py
@@ -1,6 +1,12 @@
 import whisper
 
 
-model = whisper.load_model("medium")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+def transcribe_korean_audio(file_path):
+    # 모델 로드 (한국어 포함 모델 선택)
+    model = whisper.load_model("medium")
+    result = model.transcribe(file_path)
+    print(result["text"])
+
+
+if __name__ == "__main__":
+    transcribe_korean_audio("backend/feedback/_samples/SPK064KBSCU001M001.wav")
