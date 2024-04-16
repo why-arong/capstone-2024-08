02b747fb387ea561a1784d6ae0ca281a648ee50d
Pilmo <pilmokim99@gmail.com>
2024-04-16 23:43:55 +0900

    [CD-109] Feat: add whisper module

--- backend/feedback/stt/__init__.py
+++ backend/feedback/stt/__init__.py
@@ -1,3 +1,4 @@
+
 import whisper
 
 
@@ -5,8 +6,8 @@ def transcribe_korean_audio(file_path):
     # 모델 로드 (한국어 포함 모델 선택)
     model = whisper.load_model("medium")
     result = model.transcribe(file_path)
-    print(result["text"])
+    return result["text"]
 
 
-if __name__ == "__main__":
-    transcribe_korean_audio("backend/feedback/_samples/SPK064KBSCU001M001.wav")
+# if __name__ == "__main__":
+#     transcribe_korean_audio("backend/feedback/_samples/SPK064KBSCU001M001.wav")
