faeb338af15d7201474f30663abecdc407bd92ba
anjiwon319 <anjiwon319@kookmin.ac.kr>
2024-04-19 15:31:20 +0900

    Chore: 필요없는 import 문 제거

--- front/lib/widget/script/create_user_script_button.dart
+++ front/lib/widget/script/create_user_script_button.dart
@@ -1,4 +1,3 @@
-import 'package:capstone/screen/script/create_user_script/create_user_script.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone/constants/color.dart' as colors;
 import 'package:flutter/services.dart';
@@ -12,20 +11,18 @@ ElevatedButton createUserScriptButton() {
           borderRadius: BorderRadius.circular(10),
         ),
       ),
-  onPressed: () {
-    HapticFeedback.lightImpact();
-    //Get.to(() => const CreateUserScript());
-  },
-  child: const Text(
-    '나만의 대본 만들기',
-    semanticsLabel: '나만의 대본 만들기',
-    textAlign: TextAlign.center,
-    style: TextStyle(
-      color: colors.blockColor,
-      fontSize: 13,
-      fontWeight: FontWeight.w700,
-    ),
-  )
-);
-
+      onPressed: () {
+        HapticFeedback.lightImpact();
+        //Get.to(() => const CreateUserScript());
+      },
+      child: const Text(
+        '나만의 대본 만들기',
+        semanticsLabel: '나만의 대본 만들기',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: colors.blockColor,
+          fontSize: 13,
+          fontWeight: FontWeight.w700,
+        ),
+      ));
 }
