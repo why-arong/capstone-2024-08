550b7a98545ca18728478fbb24da55151087f0a2
Pilmo <pilmokim99@gmail.com>
2024-05-06 18:51:52 +0900

    CD-166 use abs path

--- backend/voice_converison/__init__.py
+++ backend/voice_converison/__init__.py
@@ -5,9 +5,9 @@ from typing import List
 def change_voice(src: str, ref: List[str]):
     knn_vc = torch.hub.load('bshall/knn-vc', 'knn_vc', prematched=True, trust_repo=True, pretrained=True, device='cuda')
     # path to 16kHz, single-channel, source waveform
-    src_wav_path = "./SPK064KBSCU001M001.wav"
+    src_wav_path = "/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/SPK064KBSCU001M001.wav"
     # list of paths to all reference waveforms (each must be 16kHz, single-channel) from the target speaker
-    ref_wav_paths = ["./SPK014KBSCU004F002.wav"]
+    ref_wav_paths = ["/home/ubuntu/forked/capstone-2024-08/backend/voice_converison/SPK014KBSCU004F002.wav"]
 
     query_seq = knn_vc.get_features(src_wav_path)
     matching_set = knn_vc.get_matching_set(ref_wav_paths)
