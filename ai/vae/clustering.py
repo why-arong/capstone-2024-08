2375c7c77e5ab306eb481f5d24691c6a3ad76460
YunHaaaa <qlxqlrt2012@gmail.com>
2024-05-12 13:15:40 +0900

    [CD-210] Feat: add saving cluster results code

--- ai/vae/clustering.py
+++ ai/vae/clustering.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from sklearn.cluster import KMeans
 from model import VAE
 from utils import create_dataset
+import numpy as np
+
 
 # Function to generate latent space data using the VAE model
 def generate_latent_data(model, dataloader):
@@ -34,7 +36,6 @@ def kmeans_clustering(data, num_clusters):
     return cluster_centers, cluster_labels
 
 if __name__ == "__main__":
-    # Constants
     segment_length = 1024
     n_units = 256
     n_hidden_units = 64
@@ -44,20 +45,15 @@ if __name__ == "__main__":
     hop_length = 128
     num_clusters = 10
 
-    # Device
     device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-    # Load VAE model
     model_checkpoint_path = Path('ai/vae/model/ckpt')
     model = load_model_from_checkpoint(model_checkpoint_path, device)
 
-    # Create test dataloader
     test_dataloader, test_dataset_len = create_dataset("filelists/test.txt", segment_length, sampling_rate, hop_length, batch_size)
 
-    # Generate latent space data
     latent_data = generate_latent_data(model, test_dataloader)
 
-    # Perform K-means clustering
     cluster_centers, cluster_labels = kmeans_clustering(latent_data, num_clusters)
 
     # Print cluster labels and number of data points in each cluster
@@ -65,8 +61,13 @@ if __name__ == "__main__":
         num_points_in_cluster = sum(cluster_labels == cluster_id)
         print(f"Cluster {cluster_id}: {num_points_in_cluster} data points")
 
-    # Print cluster centroids and labels
     print("Cluster Centroids:")
     print(cluster_centers)
     print("Cluster Labels:")
     print(cluster_labels)
+    
+    output_file = model_checkpoint_path / "cluster_results.txt"
+    np.savetxt(output_file, cluster_centers)
+    with open(output_file, "a") as f:
+        f.write("\n")
+        np.savetxt(f, cluster_labels)
