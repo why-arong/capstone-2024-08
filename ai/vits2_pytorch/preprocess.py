8a7638f247c1ebbb4c9b9ce7cf4eee16e1598a09
YunHaaaa <qlxqlrt2012@gmail.com>
2024-05-15 10:54:13 +0900

    [CD-137] Fix: change default cleaners

--- ai/vits2_pytorch/preprocess.py
+++ ai/vits2_pytorch/preprocess.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             "filelists/ljs_audio_text_test_filelist.txt",
         ],
     )
-    parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
+    parser.add_argument("--text_cleaners", nargs="+", default=["korean_cleaners"])
 
     args = parser.parse_args()
 
