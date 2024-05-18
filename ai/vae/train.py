cc427613e138bb6db72d604d6ea3d6465b37220e
YunHaaaa <qlxqlrt2012@gmail.com>
2024-05-19 00:35:07 +0900

    [CD192] Feat: add main in train.py

--- ai/vae/train.py
+++ ai/vae/train.py
@@ -1,21 +1,31 @@
-from pathlib import Path
+import json
+import argparse
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-import itertools
+
 import os
 import time
+import itertools
+from env import AttrDict, build_env
+
 import torch
 import torch.nn.functional as F
+import torch.multiprocessing as mp
 from torch.utils.tensorboard import SummaryWriter
-from torch.utils.data import DistributedSampler, DataLoader
 from torch.distributed import init_process_group
 from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data import DistributedSampler, DataLoader
+
+
 from meldataset import MelDataset, mel_spectrogram, get_dataset_filelist
 from models import MultiPeriodDiscriminator, MultiScaleDiscriminator
 from vae import VAE, feature_loss, generator_loss, discriminator_loss
 from utils import plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
 
+
 torch.backends.cudnn.benchmark = True
+
+
 
 def train(rank, a, h):
     if h.num_gpus > 1:
@@ -228,3 +238,49 @@ def train(rank, a, h):
 
 
 
+
+def main():
+    print('Initializing Training Process..')
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--group_name', default=None)
+    parser.add_argument('--input_wavs_dir', default='LJSpeech-1.1/wavs')
+    parser.add_argument('--input_mels_dir', default='ft_dataset')
+    parser.add_argument('--input_training_file', default='LJSpeech-1.1/training.txt')
+    parser.add_argument('--input_validation_file', default='LJSpeech-1.1/validation.txt')
+    parser.add_argument('--checkpoint_path', default='cp_hifigan')
+    parser.add_argument('--config', default='')
+    parser.add_argument('--training_epochs', default=3100, type=int)
+    parser.add_argument('--stdout_interval', default=5, type=int)
+    parser.add_argument('--checkpoint_interval', default=5000, type=int)
+    parser.add_argument('--summary_interval', default=100, type=int)
+    parser.add_argument('--validation_interval', default=1000, type=int)
+    parser.add_argument('--fine_tuning', default=False, type=bool)
+
+    a = parser.parse_args()
+
+    with open(a.config) as f:
+        data = f.read()
+
+    json_config = json.loads(data)
+    h = AttrDict(json_config)
+    build_env(a.config, 'config.json', a.checkpoint_path)
+
+    torch.manual_seed(h.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(h.seed)
+        h.num_gpus = torch.cuda.device_count()
+        h.batch_size = int(h.batch_size / h.num_gpus)
+        print('Batch size per GPU :', h.batch_size)
+    else:
+        pass
+
+    if h.num_gpus > 1:
+        mp.spawn(train, nprocs=h.num_gpus, args=(a, h,))
+    else:
+        train(0, a, h)
+
+
+if __name__ == '__main__':
+    main()
